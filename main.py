b941c4e2713a00d12f337ff987b61024f12fdfd2
Robin Tainton <robintainton0509@gmail.com>
2021-12-14 01:50:22 +0200

    improve game description

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ def play():
         influencer_two=random.choice(data)
         while(influencer_one["name"] ==influencer_two["name"]):
             influencer_two=random.choice(data)
-        print("\n"+influencer_one["name"])
+        print(f"\n{influencer_one['name']}, a {influencer_one['description']} from {influencer_one['country']}")
         print(vs)
-        print(influencer_two["name"]+"\n")
+        print(f"{influencer_two['name']}, a {influencer_two['description']} from {influencer_two['country']} \n")
 
         choice = input(f"guess {influencer_one['name']} 'a' or {influencer_two['name']} 'b'").lower()
         if(choice=='a' and influencer_one["follower_count"]>influencer_two["follower_count"]):
